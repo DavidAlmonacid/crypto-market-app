92913d3a177b446431a65d09e33feabe7e5379dd
David Almonacid <david.almonacidf@gmail.com>
2023-02-21 16:15:37 -0500

    App finished
    - Search bar improved
    - TableCoins component created
    - Search by symbols added

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,14 +1,45 @@
+<script>
+  import { onMount } from 'svelte';
+
+  export let searchCoins;
+
+  let ref = null;
+
+  onMount(() => {
+    ref.focus();
+  });
+</script>
+
 <header class="header">
   <div class="header__wrapper">
     <h1 class="header__title">CRYPTO</h1>
 
     <div class="header__search-wrapper">
-      <label for="search">Search</label>
+      <label for="search">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          width="16"
+          class="header__search-icon"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </label>
+
       <input
         type="search"
         placeholder="Search coin"
         class="header__search"
         id="search"
+        on:input={searchCoins}
+        bind:this={ref}
       />
     </div>
   </div>
@@ -18,7 +49,7 @@
   .header {
     margin: 0 auto;
     padding: 32px 0;
-    max-width: 520px;
+    max-width: 538px;
   }
 
   .header__wrapper {
@@ -27,9 +58,7 @@
     align-items: center;
     border-radius: 12px;
     padding: 12px 8px;
-
-    /* change */
-    background: rebeccapurple;
+    background-color: #052c39;
   }
 
   .header__title {
@@ -39,5 +68,23 @@
   .header__search-wrapper {
     display: flex;
     align-items: center;
+    gap: 4px;
+    border-radius: 10px;
+    padding: 4px 8px;
+    height: 30px;
+    background-color: #084c61;
+  }
+
+  .header__search-icon {
+    vertical-align: middle;
+    transform: rotateY(200deg);
+  }
+
+  .header__search {
+    align-self: stretch;
+    outline: 0;
+    border: 0;
+    width: 192px;
+    background: none;
   }
 </style>
